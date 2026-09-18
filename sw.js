b9b6269d56fc6fb1c9934d016357c0608b0bcb12
lkkzy <165129636+lil22016@users.noreply.github.com>
2026-09-18 18:59:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='deadline-garden-v16-27-calendar-icons-mobile-header';
-const ASSETS=['./','./index.html','./styles.css?v=1627','./app.js?v=1627','./manifest.webmanifest','./favicon.ico?v=1612','./apple-touch-icon.png?v=1612','./icon-192.png?v=1612','./icon-512.png?v=1612'];
+const CACHE='deadline-garden-v16-28-month-markers';
+const ASSETS=['./','./index.html','./styles.css?v=1628','./app.js?v=1628','./manifest.webmanifest','./favicon.ico?v=1612','./apple-touch-icon.png?v=1612','./icon-192.png?v=1612','./icon-512.png?v=1612'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
